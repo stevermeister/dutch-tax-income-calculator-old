03fddd1155a2707d556fb1021d0af9127351f531
Stepan Suvorov <stevermeister@gmail.com>
2020-01-07 14:56:18 +0200

    fixed calculation issues

--- src/calc/calc.component.js
+++ src/calc/calc.component.js
@@ -226,10 +226,11 @@ let calcComponent = {
           if (salary.taxableYear > rulingIncome) {
             salary.taxFreeYear = salary.taxableYear * 0.30;
             salary.taxableYear -= salary.taxFreeYear;
-            if (salary.taxableYear < rulingIncome) { // For partial
-              salary.taxFreeYear = grossYear - rulingIncome;
-              salary.taxableYear = rulingIncome;
-            }
+            // this was a strange condition, I comment out it for now
+            // if (salary.taxableYear < rulingIncome) { // For partial
+            //   salary.taxFreeYear = grossYear - rulingIncome;
+            //   salary.taxableYear = rulingIncome;
+            // }
           }
         }
 
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const version = "1.5.0";
+const version = "1.5.1";
 const cacheName = `dutch-tax-income-calculator-${version}`;
 self.addEventListener('install', e => {
   const timeStamp = Date.now();
