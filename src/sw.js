7670dcbb3796b9d630c7757a03b77dab2cb793b8
Stepan Suvorov <stevermeister@gmail.com>
2020-07-23 19:48:54 +0200

    rel-follow

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const version = "2.0.3";
+const version = "2.0.4";
 const cacheName = `dutch-tax-income-calculator-${version}`;
 self.addEventListener('install', e => {
   const timeStamp = Date.now();
