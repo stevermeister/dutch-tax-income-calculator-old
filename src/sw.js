a25ef57ad77901029a13225fc104897826623324
Stepan Suvorov <stevermeister@gmail.com>
2021-05-30 15:07:53 +0300

    hotfix: senior age 65->66

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const version = "2.0.6";
+const version = "2.0.7";
 const cacheName = `dutch-tax-income-calculator-${version}`;
 self.addEventListener('install', e => {
   const timeStamp = Date.now();
